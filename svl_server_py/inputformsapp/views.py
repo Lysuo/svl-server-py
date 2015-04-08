f66c711bd49734aa7a395b834a5ddba6a0d7a6bb
Jerome Gueret <jer.gueret@gmail.com>
2015-04-08 18:17:22 +0100

    ajax work

--- svl_server_py/inputformsapp/views.py
+++ svl_server_py/inputformsapp/views.py
@@ -5,59 +5,70 @@ from datetime import datetime
 from inputformsapp.models import Language, Type, Chapter, UpdateChapter, Word
 from inputformsapp.forms import LanguageForm, TypeForm, ChapterForm, UpdateChapterForm
 
+from django.views.decorators.csrf import csrf_exempt
+
 import csv
+import json
 
 # Create your views here.
 
+@csrf_exempt
 def home(request):
+
   if request.method == 'POST':
-    formLanguage = LanguageForm(request.POST)
-    formType= TypeForm(request.POST)
-    formChapter= ChapterForm(request.POST, request.FILES)
-    formChapterUpdate= UpdateChapterForm(request.POST, request.FILES)
 
-    if formLanguage.is_valid():
+    print "before if/else"
+    if request.is_ajax():
+      print "ajax!"
+      response_data = dealAjax(request)
+      return HttpResponse(json.dumps(response_data),content_type='application/json')
 
-      language = formLanguage.cleaned_data['nameLanguage']
-      icon = formLanguage.cleaned_data['icon']
+    else:
+      formLanguage = LanguageForm(request.POST)
+      formType= TypeForm(request.POST)
+      formChapter= ChapterForm(request.POST, request.FILES)
+      formChapterUpdate= UpdateChapterForm(request.POST, request.FILES)
 
-      sentLanguage = True
-      formLanguage.save()
+      # submitting form for languages
+      if formLanguage.is_valid():
+        language = formLanguage.cleaned_data['nameLanguage']
+        icon = formLanguage.cleaned_data['icon']
+        sentLanguage = True
+        formLanguage.save()
 
-    elif formType.is_valid():
+      # submitting form for types
+      elif formType.is_valid():
+        language = formType.cleaned_data['typeLanguage']
+        name = formType.cleaned_data['nameType']
+        sentType = True
+        formType.save()
 
-      language = formType.cleaned_data['typeLanguage']
-      name = formType.cleaned_data['nameType']
+      # submitting form for chapters (new)
+      elif formChapter.is_valid():
+        language = formChapter.cleaned_data['chapterLanguage']
+        chapterType = formChapter.cleaned_data['chapterType']
+        name = formChapter.cleaned_data['nameChapter']
+        chapterFile = formChapter.cleaned_data['mFile']
+        formChapter.save()
 
-      sentType = True
-      formType.save()
+        # parsing csv to insert words
+        reader = csv.reader(chapterFile)
+        for row in reader:
+          w = Word(wordChapter=Chapter.objects.filter(nameChapter=name)[0], french=row[0], translation=row[1])
+          w.save()
+        sentChapter = True
 
-    elif formChapter.is_valid():
+      # submitting form for chapters (update)
+      elif formChapterUpdate.is_valid():
 
-      language = formChapter.cleaned_data['chapterLanguage']
-      chapterType = formChapter.cleaned_data['chapterType']
-      name = formChapter.cleaned_data['nameChapter']
-      chapterFile = formChapter.cleaned_data['mFile']
-      formChapter.save()
+        language = formChapterUpdate.cleaned_data['chapterLanguageUpdate']
+        chapterType = formChapterUpdate.cleaned_data['chapterTypeUpdate']
+        name = formChapterUpdate.cleaned_data['nameChapterUpdate']
+        chapterFile = formChapterUpdate.cleaned_data['mFileUpdate']
+        sentChapter = True
+        formChapterUpdate.save()
 
-      reader = csv.reader(chapterFile)
-
-      for row in reader:
-        w = Word(wordChapter=Chapter.objects.filter(nameChapter=name)[0], french=row[0], translation=row[1])
-        w.save()
-
-      sentChapter = True
-
-    elif formChapterUpdate.is_valid():
-
-      language = formChapterUpdate.cleaned_data['chapterLanguageUpdate']
-      chapterType = formChapterUpdate.cleaned_data['chapterTypeUpdate']
-      name = formChapterUpdate.cleaned_data['nameChapterUpdate']
-      chapterFile = formChapterUpdate.cleaned_data['mFileUpdate']
-
-      sentChapter = True
-      formChapterUpdate.save()
-
+  # GET request
   else:
     formLanguage = LanguageForm()
     formType = TypeForm()
@@ -65,3 +76,38 @@ def home(request):
     formChapterUpdate = UpdateChapterForm()
 
   return render(request, 'home.html', locals())
+
+@csrf_exempt
+def dealAjax(request):
+  if request.method == 'POST' and request.is_ajax():
+    data = request.POST
+    selectedid = data['selected-id'] 
+    value = data['value']
+    print selectedid
+    print value
+
+    if selectedid is "chapterLanguage":
+      t = Type.objects.filter(typeLanguage=value)
+      print t
+    elif selectedid is "chapterLanguageUpdate":
+      t = Type.objects.filter(typeLanguage=value)
+      print t
+    elif selectedid is "chapterTypeUpdate":
+      c = Chapter.objects.filter(chapterType=value)
+      print c
+   
+    response_data={}
+    response_data['title'] = 'Hola'
+
+#    try:
+#      response_data['title']='Hey its done ajax'
+#      response_data['message']=sentence
+
+#    except:
+#      response_data['title']='NO'
+#      response_data['message']='NO'
+
+#    print response_data
+    return response_data
+  else:
+    return HttpResponse("get response")
--- svl_server_py/static/js/own-js.js
+++ svl_server_py/static/js/own-js.js
@@ -1,49 +1,45 @@
-jQuery(function($){
-    var $myForms = $(".forms-perso");
-    $myForms.css("display", "none");
-	
-	// change the displayed form in function of the checkbox checked.
-	$('input:radio').change(
-    function(){
-        if ($(this).is(':checked')) {
-			$myForms.css("display", "none");
-			$('#'+$(this).attr('value')).css("display", "block");
-        } 
-		else {
-			$myForms.css("display", "none");
-		}
-    });
-	
-	$('.select-perso').focus(
-	function() {
-		//alert('a select has the focus');
-	});
-	
-	$('.select-perso').change(
-	function() {
-		//alert('the value of the select has been set to : ' + $(this).find("option:selected").text());
-		
-		$.ajax({
-		   url : 'more_com.php',
-		   type : 'GET',
-		   dataType : 'html',
-		   success : function(code_html, statut){        
-		   },
-		   error : function(resultat, statut, erreur){
-		   },
-		   complete : function(resultat, statut){
-		   }
-		});
-		
-		$.get(
-			'fichier_cible.php', // Le fichier cible côté serveur.
-			'false', // Nous utilisons false, pour dire que nous n'envoyons pas de données.
-			'nom_fonction_retour', // Nous renseignons uniquement le nom de la fonction de retour.
-			'text' // Format des données reçues.
-		);
+$(function(){
+  var $myForms = $(".forms-perso");
+  $myForms.css("display", "none");
 
-		function nom_fonction_retour(texte_recu){
-		}
-		
-	});		
+  // change the displayed form in function of the checkbox checked.
+  $('input:radio').change(
+    function(){
+      if ($(this).is(':checked')) {
+        $myForms.css("display", "none");
+        $('#'+$(this).attr('value')).css("display", "block");
+      } 
+      else {
+        $myForms.css("display", "none");
+      }
+    });
+
+  $('.select-form-perso').change(
+    function() {
+      //alert('the value of the select has been set to : ' + $(this).find("option:selected").text());
+      //alert($(this).attr('id'));
+
+      $.ajax({
+        type: 'POST' ,
+        url: '/',
+        contentType: "application/x-www-form-urlencoded;charset=utf-8",
+        datatype: "json",
+        async: true,
+        data: { 
+          'selected-id' : $(this).attr('id'),
+          'value' : $(this).find('option:selected').text(),
+        },
+
+        success: function(json) {
+          alert(json.title);
+        },
+        error: function(){
+          alert("error");
+        }, 
+        complete: function(){
+          alert("complete");
+        } 
+      });
+
+    });
 });
